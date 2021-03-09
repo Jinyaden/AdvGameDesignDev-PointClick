d2e5778f3fe56a27248d7ea454399ae475ff6316
Danny <do5817e@gre.ac.uk>
2021-03-09 11:55:42 +0000

    Added Wizard Character (NPC)

---
+++ PointAndClick/Assets/Scripts/NPC/SpriteTurnaround.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***********************************************************************
+
+    Simple Script to Flip the Sprite based on the Player's Position
+
+***********************************************************************/
+
+public class SpriteTurnaround : MonoBehaviour
+{
+    public GameObject playerPosition;
+    public bool SpriteIsFacingRight = false;
+
+    private Vector3 npcPosition;
+    private float npcScaleLeft;
+    private float npcScaleRight;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( SpriteIsFacingRight == false )
+        {
+            npcScaleLeft = transform.localScale.x;
+            npcScaleRight = -transform.localScale.x;
+        }
+        else
+        {
+            npcScaleLeft = -transform.localScale.x;
+            npcScaleRight = transform.localScale.x;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 npcScale = transform.localScale;
+        Vector3 npcPosition = transform.position;
+        
+        // Check to see if the Player's X Position is Greater than NPC's X Position
+        // If yes, then Flip the Sprite
+        if ( playerPosition.transform.position.x > npcPosition.x )
+        {
+            npcScale.x = npcScaleRight;
+        }
+        else
+        {
+            npcScale.x = npcScaleLeft;
+        }
+
+        transform.localScale = npcScale;
+    }
+}
