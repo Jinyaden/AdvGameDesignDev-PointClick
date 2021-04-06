73e2a6c8b07d24c9a6970ed35c28fec6bf543495
Danny <do5817e@gre.ac.uk>
2021-04-06 11:19:46 +0100

    Play As the Main Character (Demo Ready)

---
+++ PointAndClick/Assets/Scripts/Player/MainCharacterAnimationHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterAnimationHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    public GameObject playerPosition;
+    private Vector3 newPosition;
+    private Vector3 oldPosition;
+
+    private Vector3 mainCharacterScale;
+    private float scaleLeft;
+    private float scaleRight;
+
+    public bool fixSpriteLeftGlitch = true;
+    private bool spriteLeftGlitchFixed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        mainCharacterScale = transform.localScale;
+
+        scaleLeft = -transform.localScale.x;
+        scaleRight = transform.localScale.x;
+
+        spriteLeftGlitchFixed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldPosition = playerPosition.transform.position;
+
+
+        if ( oldPosition != newPosition )
+        {
+            animator.SetBool("isWalking", true);
+
+            if ( oldPosition.x < newPosition.x )
+            {
+                mainCharacterScale.x = scaleLeft;
+            }
+            else
+            {
+                mainCharacterScale.x = scaleRight;
+            }
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+
+        if (fixSpriteLeftGlitch && !spriteLeftGlitchFixed)
+        {
+            mainCharacterScale.x = scaleRight;
+            spriteLeftGlitchFixed = true;
+        }
+
+
+        newPosition = playerPosition.transform.position;
+        transform.localScale = mainCharacterScale;
+    }
+}
