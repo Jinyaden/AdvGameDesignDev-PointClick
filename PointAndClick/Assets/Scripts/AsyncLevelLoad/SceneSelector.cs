b08703eba4346fa6eff23c4219e719ad04d1f352
Jin <Jinyaden@hotmail.com>
2021-04-06 18:52:49 +0100

    Game Beta
    Ready for assets!

---
+++ PointAndClick/Assets/Scripts/AsyncLevelLoad/SceneSelector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace COMP1288.PointClick.Jin
+{
+    enum loadlocations
+    {
+        Forest, Beach, Mountain, Forest_Outer_Ruin, Inner_Ruin,Dungeon, Secret_Dungeon_Door
+    }
+
+    //Scenes to go to are Beach, Forest, Mountain, Outer Ruins, Inner Ruins
+    public class SceneSelector : MonoBehaviour, IInteractor
+    {
+        [SerializeField] loadlocations locations;
+        private Loader sceneLoader => FindObjectOfType<Loader>();
+
+        public void Interact()
+        {
+            LoadSelectedScene();
+        }
+
+        //TODO: load scene depending on enum
+        //TODO: Set player positions depending on enum place
+
+        private void LoadSelectedScene()
+        {
+            switch (locations)
+            {
+                case loadlocations.Forest:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("Forest"));
+                    break;
+                case loadlocations.Beach:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("Beach"));
+                    break;
+                case loadlocations.Mountain:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("Mountain"));
+                    break;
+                case loadlocations.Forest_Outer_Ruin:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("OuterRuins"));
+                    break;
+                case loadlocations.Inner_Ruin:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("InnerRuins"));
+                    break;
+                case loadlocations.Dungeon:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("Dungeon"));
+                    break;
+                case loadlocations.Secret_Dungeon_Door:
+                    StartCoroutine(sceneLoader.LoadSceneWithDissolve("InnerRuins"));
+                    break;
+
+            }
+        }
+
+    }
+}
---
+++ PointAndClick/Assets/Scripts/Interactions/GateUnlocker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace COMP1288.PointClick.Jin
+{
+    public class GateUnlocker : MonoBehaviour, IInteractor
+    {
+        public void Interact()
+        {
+            //Unlock Gate if rusty key is in player prefs
+            if (PlayerPrefs.GetInt("Rusted Key") == 1)
+            {
+                //Open Gate
+                Debug.Log("Opening Gate!");
+            }
+        }
+    }
+}
