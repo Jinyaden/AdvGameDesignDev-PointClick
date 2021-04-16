e84d3671b80ce18f48310af0f095acc34ac4ef7a
David Brittain <davidbrit@live.com>
2021-04-16 13:55:05 +0100

    Added a script to give varying colours in the cloud
    It only does it on startup, putting the code in update will make it work as intended. Clean up the code and change the texture in Photoshop to have a consistent colour to begin with.

---
+++ PointAndClick/Assets/David Brittain/Visual Effects/cloudColourGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class cloudColourGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect vfx;
+
+    [SerializeField]
+    private Gradient gradientCloud = null;
+
+    [SerializeField]
+    private Gradient colourCloud1;
+
+    [SerializeField]
+    private Gradient colourCloud2;
+
+    [SerializeField]
+    private Gradient colourCloud3;
+
+    GradientColorKey[] colourKey;
+    GradientAlphaKey[] alphaKey;
+    
+    public Gradient currentColourOfCloud
+    {
+        get { return gradientCloud; }
+        set { gradientCloud = value; }
+    }
+
+     void Start()
+    {
+        gradientCloud = new Gradient();
+
+        colourKey = new GradientColorKey[3];
+        colourKey[0].color = Color.white;
+        colourKey[1].color = Color.grey;
+        colourKey[2].color = Color.black;
+
+        alphaKey = new GradientAlphaKey[0];
+
+        int newcolourofCloud;
+        newcolourofCloud = Random.Range(1, 3);
+        if (newcolourofCloud == 1)
+        {
+            currentColourOfCloud = colourCloud1;
+        }
+        if (newcolourofCloud == 2)
+        {
+            currentColourOfCloud = colourCloud2;
+        }
+        if (newcolourofCloud == 3)
+        {
+            currentColourOfCloud = colourCloud3;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vfx.SetGradient("colourCloud", gradientCloud);
+        vfx.SetGradient("First colour of cloud", colourCloud1);
+        vfx.SetGradient("Second colour of cloud", colourCloud2);
+        vfx.SetGradient("Third colour of cloud", colourCloud3);
+
+    }
+}
